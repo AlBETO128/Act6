65a23b8ca0dd0a2c1bd64cfe07ae3fb5fdb43877
ALBERTO_CARBAJAL_VAZQUEZ <a.21308051280449@cbtis128.edu.mx>
2024-05-21 04:38:19 +0000

    update

---
+++ lib/data/product_data.dart
@@ -0,0 +1,37 @@
+import 'package:act6Carbajal/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+    img: "assets/3.jpg",
+    id: "id: 2",
+    nombre: "nombre: Cuchara para Cemento",
+    precio: "precio: 160",
+    modelo: "modelo: t65",
+    marca: "marca: TRUPER",
+    color: "color: Naranja",
+    region: "region: Mexico",
+    categoria: "categoria: Abañileria",
+  ),
+  ProductModel(
+    img: "assets/3.jpg",
+    id: "id: 2",
+    nombre: "nombre: Cuchara para Cemento",
+    precio: "precio: 160",
+    modelo: "modelo: t65",
+    marca: "marca: TRUPER",
+    color: "color: Naranja",
+    region: "region: Mexico",
+    categoria: "categoria: Carpinteria",
+  ),
+  ProductModel(
+    img: "assets/4.jpg",
+    id: "id: 3",
+    nombre: "nombre Cuchara para Cemento",
+    precio: "precio: 160",
+    modelo: "modelo: t65",
+    marca: "marca: TRUPER",
+    color: "color: Naranja",
+    region: "region: Mexico",
+    categoria: "categoria: Jardineria",
+  ),
+];
